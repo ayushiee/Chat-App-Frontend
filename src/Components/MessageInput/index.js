43b75c05fea87f50e6014a677407b0aa4856c629
Ayushi <ayushigupta8@gmail.com>
2020-08-30 18:06:29 +0530

    Added basic bot replies

--- src/Components/MessageInput/index.js
+++ src/Components/MessageInput/index.js
@@ -1,11 +1,35 @@
-import React from 'react';
-import './MessageInput.css';
+import React, { useState } from "react";
+import "./MessageInput.css";
 
-export default function MessageInput() {
-    return (
-        <div className='msgInputContainer'>
-            <input type='text' className='textInput' placeholder='Type a message' />
-            <button type='submit' className='sendButton'>Send</button>
-        </div>
-    )
-};
+export default function MessageInput({ onSend }) {
+  const [input, setInput] = useState("");
+
+  const onSubmit = () => {
+    onSend(input);
+    setInput("");
+  };
+
+  const onInputChange = ({ target }) => setInput(target.value);
+
+  const onEnter = ({ key }) => {
+    if (key === "Enter") {
+      onSubmit();
+    }
+  };
+
+  return (
+    <div className="msgInputContainer">
+      <input
+        value={input}
+        onChange={onInputChange}
+        onKeyDown={onEnter}
+        type="text"
+        className="textInput"
+        placeholder="Type a message"
+      />
+      <button type="submit" onClick={onSubmit} className="sendButton">
+        Send
+      </button>
+    </div>
+  );
+}
--- src/Containers/MainContainer/index.js
+++ src/Containers/MainContainer/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 import MessageInput from "../../Components/MessageInput";
 import TextComponent from "../../Components/TextComponent";
@@ -6,18 +6,55 @@ import TitleBar from "../../Components/TitleBar";
 
 import "./MainContainer.css";
 
+const makeMessage  = (userId, content) => ({userId, content});
+
+const getBotReply = message => {
+  const botId = 'bot-user';
+  let content = 'I do not know what to say';
+
+  if (message.includes('hello')) {
+    content = 'well, hello there'
+  }
+
+  return makeMessage(botId, content);
+}
+
 function MainContainer() {
+  const user = {
+    id: 'user-me'
+  };
+
+  const [messages, setMessages] = useState([]);
+
+  const onSend = content => {
+    const message = makeMessage(user.id, content);
+    const botMessage = getBotReply(content);
+
+    setMessages(messages => {
+      const updatedMessages = [...messages];
+      updatedMessages.push(message)
+      updatedMessages.push(botMessage);
+      console.log(updatedMessages);
+      return updatedMessages;
+    });
+  };
+
+  const renderMessages = () => messages.map((message, index) => {
+      const { userId, content } = message;
+      const isMe = userId === user.id;
+
+      return <TextComponent key={index} text={content} isMe={isMe} />
+    })
+
   return (
     <div className="mainContainer">
       <div className="chatWindow">
         <TitleBar />
         <div className="texts">
-          <TextComponent text="helloooo" isMe />
-          <TextComponent text="helloooo" isMe={false} />
-          <TextComponent text="helloooo" isMe />
+          {renderMessages()}
         </div>
         <div className="inputTexts">
-          <MessageInput />
+          <MessageInput onSend={onSend} />
         </div>
       </div>
     </div>
